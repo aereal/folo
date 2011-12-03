cf5333577be21b929121009851d1ff30670f6d3e
aereal <aereal@kerare.org>
2011-12-04 05:18:50 +0900

    add HatenaFotolifeItem as embedded document

--- models/hatena_fotolife_item.rb
+++ models/hatena_fotolife_item.rb
@@ -6,17 +6,14 @@ class HatenaFotolifeItem
   field :hatena_id, :type => String
   field :entry_id, :type => String
 
-  # You can define indexes on documents using the index macro:
-  # index :field <, :unique => true>
-
-  # You can create a composite key in mongoid to replace the default id using the key macro:
-  # key :field <, :another_field, :one_more ....>
+  # relations
+  embedded_in :item
 
   def entry_path
     [Time.parse(entry_id).strftime('%Y%m%d'), entry_id].join('/')
   end
 
-  def resource_url(format)
-    "http://img.f.hatena.ne.jp/images/fotolife/#{hatena_id.first}/#{hatena_id}/#{entry_path}.#{format}"
+  def resource_url
+    "http://img.f.hatena.ne.jp/images/fotolife/#{hatena_id.first}/#{hatena_id}/#{entry_path}.#{item.format}"
   end
 end
--- models/item.rb
+++ models/item.rb
@@ -4,10 +4,8 @@ class Item
 
   # field <name>, :type => <type>, :default => <value>
   field :title, :type => String
+  field :format, :type => String
 
-  # You can define indexes on documents using the index macro:
-  # index :field <, :unique => true>
-
-  # You can create a composite key in mongoid to replace the default id using the key macro:
-  # key :field <, :another_field, :one_more ....>
+  # relations
+  embeds_one :hatena_fotolife_item
 end
