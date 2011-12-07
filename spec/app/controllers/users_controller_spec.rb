7ed5eb658281484dce2098c59a8adde2476fa6fe
aereal <aereal@kerare.org>
2011-12-08 02:18:35 +0900

    users#show takes user's nickname

--- spec/app/controllers/users_controller_spec.rb
+++ spec/app/controllers/users_controller_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe "UsersController" do
-  before do
-    get "/"
+  subject do
+    last_response
   end
 
-  it "returns hello world" do
-    last_response.body.should == "Hello World"
+  let(:user) do
+    Fabricate(:user)
+  end
+
+  describe "#index" do
+    before do
+      get app.url_for(:users, :index)
+    end
+
+    it "is OK" do
+      should be_ok
+    end
+  end
+
+  describe "#show" do
+    before do
+      get app.url_for(:users, :show, nickname: user)
+    end
+
+    it "is OK" do
+      should be_ok
+    end
   end
 end
