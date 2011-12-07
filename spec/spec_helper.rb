52622c89d7935930bc64dca120a744564c52d8e9
aereal <aereal@kerare.org>
2011-12-08 02:30:56 +0900

    setup for Guard x Spork x RSpec x Padrino

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,27 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
-require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
-RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
-  conf.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.orm = 'mongoid'
+Spork.prefork do
+  RSpec.configure do |conf|
+    conf.include Rack::Test::Methods
+
+    conf.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.orm = 'mongoid'
+    end
+
+    conf.after(:suite) do
+      DatabaseCleaner.clean
+    end
   end
-  conf.after(:suite) do
-    DatabaseCleaner.clean
+
+  def app
+    ##
+    # You can handle all padrino applications using instead:
+    #   Padrino.application
+    Folo.tap { |app|  }
   end
 end
 
-def app
-  ##
-  # You can handle all padrino applications using instead:
-  #   Padrino.application
-  Folo.tap { |app|  }
+Spork.each_run do
+  require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 end
